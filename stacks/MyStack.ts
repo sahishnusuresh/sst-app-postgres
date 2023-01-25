1c6b8d2cb556f40d4a2ca5db3aa16283139099d6
sahishnusuresh <sahishnusuresh@gmail.com>
2023-01-25 16:23:35 +0530

    added postgres table

---
+++ stacks/MyStack.ts
@@ -0,0 +1,30 @@
+import { Api, RDS, StackContext } from "@serverless-stack/resources";
+
+export function MyStack({ stack }: StackContext) {
+  const DATABASE = "CounterDB";
+
+  // Create the Aurora DB cluster
+  const cluster = new RDS(stack, "Cluster", {
+    engine: "postgresql10.14",
+    defaultDatabaseName: DATABASE,
+    migrations: "services/migrations",
+  });
+  // Create a HTTP API
+const api = new Api(stack, "Api", {
+  defaults: {
+    function: {
+      bind: [cluster],
+    },
+  },
+  routes: {
+    "POST /": "functions/lambda.handler",
+  },
+});
+
+// Show the resource info in the output
+stack.addOutputs({
+  ApiEndpoint: api.url,
+  SecretArn: cluster.secretArn,
+  ClusterIdentifier: cluster.clusterIdentifier,
+});
+}
